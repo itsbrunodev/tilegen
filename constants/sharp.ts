bbd308ebc72a4e5006716bc7a73febc1d33f62fc
itsbrunodev <brunka215@gmail.com>
2025-05-29 23:57:28 +0200

    chore: add limitInputPixels note

--- constants/sharp.ts
+++ constants/sharp.ts
@@ -1,5 +1,8 @@
 import type { SharpOptions } from "sharp";
 
 export const SHARP_CONFIG = {
+	/**
+	 * Disable the default input pixel limit of 268402689.
+	 */
 	limitInputPixels: false,
 } as SharpOptions;
